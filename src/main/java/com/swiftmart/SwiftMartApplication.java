dcbc7d740e1dc6fa69a331c082f997fcbd8fc09a
Bùi Hữu Lộc <feature451@gmail.com>
2023-12-09 18:49:45 +0700

    Init Java Spring

---
+++ src/main/java/com/swiftmart/SwiftMartApplication.java
@@ -0,0 +1,13 @@
+package com.swiftmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftMartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swiftmart/SwiftMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftMartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
